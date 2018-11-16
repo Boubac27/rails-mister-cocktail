268477e0a465e95efac6fa1b8e247c4e7d85ab98
BA <ba.boubacar27@gmail.com>
2018-11-16 12:04:07 +0100

    ajout root to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   resources :doses
+  resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'cocktails/new', to: 'cocktails#new', as: :new
   get 'cocktails/:id', to: 'cocktails#show', as: :task
@@ -8,5 +10,4 @@ Rails.application.routes.draw do
   get 'cocktails/:id/edit', to: 'cocktails#edit'
   patch 'cocktails/:id', to: 'cocktails#update'
   delete 'cocktails/:id', to: 'cocktails#destroy', as: :delete
-  resources :cocktails
 end
